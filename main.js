d979fd3823df9bd6ecd4c2d3e2708cfe70ca6d17
alexpaunero <103605819+alexpaunero@users.noreply.github.com>
2024-01-23 21:14:04 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // TODO: Add your import statements here.
 import {getRoles, getCompanies} from '/modules/salaryData.js';
 import {getAverageSalaryByRole, getAverageSalaryByCompany, getSalaryAtCompany, getIndustryAverageSalary} from '/modules/workAroundModule.js'
+import formatNumber from '/modules/utilities.js';
 
 // TODO: Get the companies and roles using the salaryData module.
 const companies = getCompanies();
